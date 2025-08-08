28c6bde65e2d88d7c72754fb76f4fd1a5bbc5cc8
dracoptera <dracoptera@gmail.com>
2025-08-08 15:22:25 -0300

    update martin pescador

--- src/data/birds/orders/coraciiformes.ts
+++ src/data/birds/orders/coraciiformes.ts
@@ -6,19 +6,16 @@ export const coraciiformes: Bird[] = [
     commonName: 'Martín pescador grande',
     englishName: 'Ringed Kingfisher',
     scientificName: 'Megaceryle torquata',
+    size: '36 cm',
     family: 'Alcedinidae',
     order: 'Coraciiformes (Martín pescador)',
     habitat: ['bañado 💧', 'costa 🌊'],
     status: '🏠 residente',
     origin: 'autóctona',
-    commonness: {
-      'Rocha': 'abundante',
-      'Maldonado': 'común',
-      'Treinta y Tres': 'común',
-      'Lavalleja': 'poco común',
-      'Rivera': 'rara',
-      'Artigas': 'muy rara'
-    },
-    ebirdEmbedUrl: 'https://macaulaylibrary.org/asset/174809481/embed'
+    conservationStatus: 'Preocupación menor',
+    commonness: "común",
+    departamentos: ['Montevideo', 'Canelones', 'San José', 'Colonia', 'Soriano', 'Río Negro', 'Paysandú', 'Salto', 'Artigas', 'Rivera', 'Tacuarembó', 'Durazno', 'Flores', 'Florida', 'Lavalleja', 'Maldonado', 'Rocha', 'Treinta y Tres', 'Cerro Largo'],
+    ebirdEmbedUrl: 'https://macaulaylibrary.org/asset/174809481/embed',
+    soundUrl: 'https://macaulaylibrary.org/asset/211555/embed',
   },
 ];
