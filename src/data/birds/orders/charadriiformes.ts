86c5a8bdf4c64580ea7758bbc59a0145c7846809
dracoptera <dracoptera@gmail.com>
2025-08-08 14:54:47 -0300

    add tero info

--- src/data/birds/orders/charadriiformes.ts
+++ src/data/birds/orders/charadriiformes.ts
@@ -6,12 +6,21 @@ export const charadriiformes: Bird[] = [
     commonName: 'Tero',
     englishName: 'Southern Lapwing',
     scientificName: 'Vanellus chilensis',
+    size: '32 cm',
     family: 'Charadriidae',
     order: 'Charadriiformes (Playeras)',
     habitat: ['pradera 🌿', 'sierra 🏔️', 'ciudad 🌆'],
     status: '🏠 residente',
     origin: 'autóctona',
     commonness: 'abundante',
-    ebirdEmbedUrl: 'https://macaulaylibrary.org/asset/442325471/embed'
+    conservationStatus: 'Preocupación menor',
+    departamentos: ['Montevideo', 'Canelones', 'San José', 'Colonia', 'Soriano', 'Río Negro', 'Paysandú', 'Salto', 'Artigas', 'Rivera', 'Tacuarembó', 'Durazno', 'Flores', 'Florida', 'Lavalleja', 'Maldonado', 'Rocha', 'Treinta y Tres', 'Cerro Largo'],
+    ebirdEmbedUrl: 'https://macaulaylibrary.org/asset/442325471/embed',
+    cardHeight: { xs: 680, lg: 780, xl: 780 },
+    soundUrl: 'https://macaulaylibrary.org/asset/189516/embed',
+    description: {
+      general: 'Una de las aves más representativas de Uruguay gran parte de Sudamérica. Su inconfundible vocalización es una onomatopeya de su nombre: "terotero", que se escucha por todo el país.',
+      curiosities: 'Son muy territoriales. Poseen espolones en las alas, que utilizan para defender su territorio y nido. Una de sus estrategias de protección de nido es simular que está en otro lugar, y hasta simular estar heridos para ser una presa más fácil que su nido.',
+    }
   },
 ];
