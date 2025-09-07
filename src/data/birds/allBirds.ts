6ab5e26e50042fdb2b017ff23c0675b414ef3f58
dracoptera <dracoptera@gmail.com>
2025-09-07 13:31:46 -0300

    reorder orders

--- src/data/birds/allBirds.ts
+++ src/data/birds/allBirds.ts
@@ -1,41 +1,41 @@
 import { Bird } from "./types";
-import { passeriformes } from "./orders/passeriformes";
-import { anseriformes } from "./orders/anseriformes";
-import { pelecaniformes } from "./orders/pelecaniformes";
 import { accipitriformes } from "./orders/accipitriformes";
+import { anseriformes } from "./orders/anseriformes";
 import { charadriiformes } from "./orders/charadriiformes";
-import { columbiformes } from "./orders/columbiformes";
-import { psittaciformes } from "./orders/psittaciformes";
-import { piciformes } from "./orders/piciformes";
-import { coraciiformes } from "./orders/coraciiformes";
-import { rheiformes } from "./orders/rheiformes";
-import { tinamiformes } from "./orders/tinamiformes";
-import { galliformes } from "./orders/galliformes";
-import { podicipediformes } from "./orders/podicipediformes";
-import { phoenicopteriformes } from "./orders/phoenicopteriformes";
-import { sphenisciformes } from "./orders/sphenisciformes";
-import { procellariiformes } from "./orders/procellariiformes";
 import { ciconiiformes } from "./orders/ciconiiformes";
+import { columbiformes } from "./orders/columbiformes";
+import { coraciiformes } from "./orders/coraciiformes";
+import { galliformes } from "./orders/galliformes";
+import { passeriformes } from "./orders/passeriformes";
+import { pelecaniformes } from "./orders/pelecaniformes";
+import { phoenicopteriformes } from "./orders/phoenicopteriformes";
+import { piciformes } from "./orders/piciformes";
+import { podicipediformes } from "./orders/podicipediformes";
+import { procellariiformes } from "./orders/procellariiformes";
+import { psittaciformes } from "./orders/psittaciformes";
+import { rheiformes } from "./orders/rheiformes";
+import { sphenisciformes } from "./orders/sphenisciformes";
+import { tinamiformes } from "./orders/tinamiformes";
 
 // All order files are properly imported and available
 
 // Combine all birds from different orders
 export const uruguayBirds: Bird[] = [
-  ...passeriformes,
-  ...anseriformes,
-  ...pelecaniformes,
   ...accipitriformes,
+  ...anseriformes,
   ...charadriiformes,
-  ...columbiformes,
   ...ciconiiformes,
-  ...psittaciformes,
-  ...piciformes,
+  ...columbiformes,
   ...coraciiformes,
-  ...rheiformes,
-  ...tinamiformes,
   ...galliformes,
-  ...podicipediformes,
+  ...passeriformes,
+  ...pelecaniformes,
   ...phoenicopteriformes,
-  ...sphenisciformes,
+  ...piciformes,
+  ...podicipediformes,
   ...procellariiformes,
+  ...psittaciformes,
+  ...rheiformes,
+  ...sphenisciformes,
+  ...tinamiformes,
 ];
